dc5c3f477c5f3cf58b74bb075b15812ea4398624
Sean King <king4sn@mail.uc.edu>
2026-02-24 08:13:03 -0500

    Add placeholder lab07.py for CI

---
+++ week07/lab07.py
@@ -0,0 +1,16 @@
+# Placeholder lab07.py
+# The CI workflow for week07 checks for the existence of this file before
+# running the tests.  It doesn't need to contain any logic for this lab, but
+# we import the two primary modules here so that running this file exercises
+# them at least minimally.
+
+from lab07_contact_book import save_contacts_to_json, load_contacts_from_json
+from lab07_api_client import get_api_data
+
+
+def main():
+    print("Lab 07 placeholder module. Modules have been imported successfully.")
+
+
+if __name__ == '__main__':
+    main()
